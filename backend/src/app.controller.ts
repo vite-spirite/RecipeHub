d4803cc90386292aea13c2462bd540ba39bd0f61
ViteSpirite <boulangermatheo62@gmail.com>
2023-07-20 13:40:00 +0200

    add swagger

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiProperty } from '@nestjs/swagger';
+
+class TestDTO {
+  @ApiProperty()
+  id: number
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +14,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  test(@Query('num') num: number): number {
+    return num;
+  }
+
+  @Post('/create')
+  create(@Body() c: TestDTO): number {
+    return c.id;
   }
 }
