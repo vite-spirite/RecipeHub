51febe82b1df4171f8b02d60cda78193393349b9
ViteSpirite <boulangermatheo62@gmail.com>
2023-07-22 12:21:31 +0200

    add config module and update exemple.env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,19 +6,25 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import {redisStore} from 'cache-manager-redis-yet'
 import type { RedisClientOptions } from 'redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+
     CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
       isGlobal: true,
-      useFactory: async () => ({
+      useFactory: async (config: ConfigService) => ({
         store: await redisStore({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host: config.get('REDIS_HOST'),
+            port: parseInt(config.get('REDIS_PORT')),
           },
-          password: 'redis',
-          ttl: -1
+          database: parseInt(config.get('REDIS_REFRESH_TOKEN_DB')),
+          password: config.get('REDIS_PASSWORD'),
+          ttl: -1,
         }),
       }),
     }),
