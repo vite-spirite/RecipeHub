4efd1eb22adcc02d30e6dc1c1de9e8ec0a433f61
ViteSpirite <boulangermatheo62@gmail.com>
2023-07-22 12:08:52 +0200

    add redis caching

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
+import {redisStore} from 'cache-manager-redis-yet'
+import type { RedisClientOptions } from 'redis';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    CacheModule.registerAsync<RedisClientOptions>({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+          password: 'redis',
+          ttl: -1
+        }),
+      }),
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
