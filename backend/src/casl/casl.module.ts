b20e7eb68735b3b928a18d7b37558d81dfa77255
ViteSpirite <boulangermatheo62@gmail.com>
2023-07-23 21:25:58 +0200

    add caching permissions

--- backend/src/casl/casl.module.ts
+++ backend/src/casl/casl.module.ts
@@ -3,8 +3,28 @@ import { CaslAbilityFactory } from './casl-ability.factory/casl-ability.factory'
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CaslController } from './casl.controller';
 import { CaslService } from './casl.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
+    imports: [
+        ConfigModule,
+        CacheModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                store: await redisStore({
+                    socket: {
+                        host: config.get('REDIS_HOST'),
+                        port: parseInt(config.get('REDIS_PORT')),
+                    },
+                    database: parseInt(config.get('REDIS_CACHE_ROLE_DB')),
+                    password: config.get('REDIS_PASSWORD'),
+                }),
+            }),
+        }),
+    ],
     controllers: [CaslController],
     providers: [CaslService, CaslAbilityFactory, PrismaService],
     exports: [CaslAbilityFactory],
--- backend/src/casl/casl.service.ts
+++ backend/src/casl/casl.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from 'src/users/entities/user.entity';
 import { Permission } from './entities/permission.entity';
+import { CACHE_MANAGER, CacheStore } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CaslService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, @Inject(CACHE_MANAGER) private cache: CacheStore, private config: ConfigService) {}
     
     async getPermissions(user?: User): Promise<Permission[]> {
+
+        const cache = user ? await this.cache.get<Permission[]>(`permissions:${user.id}`) : await this.cache.get<Permission[]>('permissions:anonymous');
+
+        if(cache) {
+            return cache;
+        }
+    
         const userRoles = user ? await this.prisma.user.findUnique({where: {id: user.id}, include: {roles: {include: {permissions: true}}}}) : undefined;
         const roles = user ? userRoles.roles : await this.prisma.role.findMany({include: {permissions: true}, where: {name: 'anonymous'}});
         const permissions = roles.flatMap(role => role.permissions);
-        return permissions.map<Permission>(permission => ({action: permission.action, subject: permission.subject, condition: permission.condition as string, id: permission.id, roleId: permission.roleId}));
+        const permissionMapped = permissions.map<Permission>(permission => ({action: permission.action, subject: permission.subject, condition: permission.condition as string, id: permission.id, roleId: permission.roleId}));
+
+        const day = user ? parseInt(this.config.get('REDIS_CACHE_ROLE_TTL')) : parseInt(this.config.get('REDIS_CACHE_ROLE_ANONYMOUS_TTL'));
+        const ttl = 60*60*24*day;
+
+        await this.cache.set(`permissions:${user ? user.id : 'anonymous'}`, permissionMapped, ttl);
+        return permissionMapped;
+    }
+
+    async clearCache(user?: User) {
+        await this.cache.del(`permissions:${user ? user.id : 'anonymous'}`);
     }
 }
